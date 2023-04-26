ecc1b0bd58e4b2bc413add23232ec3de57b0818c
Vadim Smirnov <98453684+VadimSmirnov213@users.noreply.github.com>
2023-04-26 21:41:34 +0300

    Create KEYBOARD.py

---
+++ KEYBOARD.py
@@ -0,0 +1,22 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+button4 = KeyboardButton('name_button')
+button5 = KeyboardButton('name_button')
+button6 = KeyboardButton('name_button')
+button7 = KeyboardButton('name_button')
+button8 = KeyboardButton('name_button')
+
+button9 = KeyboardButton('name_button')
+button10 = KeyboardButton('name_button')
+button11 = KeyboardButton('name_button')
+
+button12 = KeyboardButton('///')
+
+
+markup_big = ReplyKeyboardMarkup()
+
+markup_big.add(button12)
